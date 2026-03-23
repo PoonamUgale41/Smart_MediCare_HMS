a99df01e022ada9c888a032dde64c20a14c7b2f0
PoonamUgale41 <ugalepoonam41@gmail.com>
2026-03-24 00:49:18 +0530

    info added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let doctors = document.querySelectorAll(".team")
+let doctors = document.querySelectorAll(".team");
+let count = 0;
+
+doctors.forEach(function(card,index){
+    card.style.left=`${index*100}%`;
+});
+
+function myFun(){
+    doctors.forEach(function(card){
+        card.style.transform=`translatex(-${count * 100}%)`;
+    });
+}
+
+setInterval(function(){
+    count++;
+    if(count == doctors.length){
+        count=0;
+    }
+    myFun();
+}, 3000);
